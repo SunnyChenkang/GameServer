8ba870ae5d646bb1458cfbba24c4fb28ba9e83c0
ioi <296679685@qq.com>
2019-07-16 11:01:46 +0800

    目录整理；

--- code/EVA/server/server_share/redis/db_redis_def.h
+++
@@ -1,9 +0,0 @@
-#ifndef SS_DBREDIS_DEF_H_
-#define SS_DBREDIS_DEF_H_
-
-#include <server_share/common/common.h>
-
-// ”Œœ∑’Ωº®KEY;
-const NLMISC::CSString REDIS_GAME_RECORD = "REDIS_GAME_RECORD";
-
-#endif//SS_DBREDIS_DEF_H_
